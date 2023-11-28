7ade7fac8c7e753a163716a04839de94cc1ca408
lflanagan17 <l_flanagan@coloradocollege.edu>
2023-11-28 12:14:24 -0700

    skeleton for home page with login button

--- app/page.js
+++ app/page.js
@@ -2,11 +2,31 @@
 
 import Image from 'next/image'
 
+function LoginButton() {
+   const buttonStyle = {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '40px', // Set a specific height for the button if needed
+    };
+    return <button style={buttonStyle}>Sign In</button>;
+ }
+ 
+
 export default function Home() {
  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>here is my website everybody. STEW ON IT</h1>
-      <p>it took me outrageously long to figure out how to do this</p>
-    </main> 
+   //flex min-h-screen 
+   <main className="flex flex-col items-center justify-between p-24">
+      <h1>Welcome to TigerList</h1>
+      <p>a program connect Colorado College students looking to exchange goods, services, and reduce waste.</p>
+      <div className="flex items-center justify-center mt-20"> {/* Adjust mt-8 based on your preference */}
+         <p> </p>
+      </div>
+      <div className="flex items-center justify-center mt-20"> {/* Adjust mt-8 based on your preference */}
+         <LoginButton />
+      </div>
+   </main>
  )
 }
+
+// sign in page with AUTHENTICATION 
