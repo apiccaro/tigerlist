553946330f55dc57a0696fca1e08110f0528b414
lflanagan17 <l_flanagan@coloradocollege.edu>
2023-11-28 13:43:44 -0700

    login skeleton with button

--- app/page.js
+++ app/page.js
@@ -4,13 +4,16 @@ import Image from 'next/image'
 
 function LoginButton() {
    const buttonStyle = {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center', 
-      height: '40px', // Set a specific height for the button if needed
-    };
-    return <button style={buttonStyle}>Sign In</button>;
- }
+     display: 'flex',
+     alignItems: 'center',
+     justifyContent: 'center',
+     height: '40px', // Set a specific height for the button if needed
+     border: '2px solid white', // Add a white border
+     padding: '0 16px', // Add padding for better visual appearance
+   };
+ 
+   return <button style={buttonStyle}>Sign In</button>;
+ } 
  
 
 export default function Home() {
@@ -18,7 +21,7 @@ export default function Home() {
    //flex min-h-screen 
    <main className="flex flex-col items-center justify-between p-24">
       <h1>Welcome to TigerList</h1>
-      <p>a program connect Colorado College students looking to exchange goods, services, and reduce waste.</p>
+      <p>a program connecting Colorado College students looking to exchange goods, services, and reduce waste.</p>
       <div className="flex items-center justify-center mt-20"> {/* Adjust mt-8 based on your preference */}
          <p> </p>
       </div>
