3a6f3eacbc24ac5e3ea6924a742343392f07c1dd
jacksondresser <j_dresser@coloradocollege.edu>
2023-11-28 09:38:14 -0700

    File name change and product view attempt

---
+++ app/productview/page.js
@@ -0,0 +1,48 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+
+        <div className="fixed flex h-48 w-full items-end justify-center lg:static lg:h-auto lg:w-auto lg:bg-none">
+
+            <p className="text-6xl font-semibold">
+                <code className="font-mono font-bold">TigerList</code>
+            </p>
+
+        </div>
+
+        <div className="fixed flex h-48 w-full items-end justify-center lg:static lg:h-auto lg:w-auto lg:bg-none">
+
+            <p className="text-4xl font-semibold">
+                <code className="font-mono font-bold">Product Name</code>
+            </p>
+
+        </div>
+
+      </div>
+
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+
+        <p className="text-2xl font-semibold">
+          <code className="font-mono font-bold">Product Information</code>
+          {/* <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>a
+            Item description will populate here
+          </p> */}
+
+        </p>
+        
+        <p className="text-2xl font-semibold">
+          <code className="font-mono font-bold">Contact</code>
+          {/* <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Contact options and resources will populate here
+          </p> */}
+
+        </p>
+
+      </div>
+    </main>
+  )
+}
