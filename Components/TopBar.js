57c8e4c902049e904783daff6eee7fb117ede584
jacksondresser <j_dresser@coloradocollege.edu>
2023-11-29 09:51:03 -0700

    Small changes

--- Components/TopBar.js
+++ Components/TopBar.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function TB() {
     return(
         <aside className="text-white p-4 text-6xl font-bold mb-5 bg-black">
-            <Link href={'/'} className="flex">
+            <Link href={'/listview'} className="flex">
                 TigerList
                 <Image
                 src="/46710.svg"
