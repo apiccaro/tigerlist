ca6c49121449357cef0ac9decafd469b3e2b92cf
jacksondresser <j_dresser@coloradocollege.edu>
2023-12-05 09:22:42 -0700

    Updated image layout and UI adjustments

--- Components/BuyProducts.js
+++ Components/BuyProducts.js
@@ -6,8 +6,10 @@ import Image from "next/image";
 import Link from 'next/link';
 import BuyProductBox from "./BuyProductBox";
 
-// This youtube video provided grid formatting help https://www.youtube.com/watch?v=dTFXufTgfOE
+// This will be the list to hold all of the product dictionaries
+var allListings;
 
+// This youtube video provided grid formatting help https://www.youtube.com/watch?v=dTFXufTgfOE
 const ProductsGrid = styled.div`
     margin-top: 20px;
     margin-left: 20px;
@@ -30,3 +32,17 @@ export default function Products(){
         </ProductsGrid>
     );
 }
+
+// export default function DisplayProducts(){
+//     return(
+//         // Some function call to populate the allListings list.
+//         // allListings = database.getAllPosts();
+
+//         // For all of the dictionaries in the list call the BuyProductBox function with dictionaryt values as parameters
+//         // <ProductsGrid className="flex flex-grow">
+//         //     for (listing in allListings) {
+//         //         <BuyProductBox(listing.title, listing.price, listing.description, listing.category, listing.condition, listing.location, listing.email, listing.phone, listing.image) />;
+//         //     }
+//         // </ProductsGrid>
+//     );
+// }
