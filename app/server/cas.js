f311efb7629b1f31d1fd8757302b8b0ea7220651
lflanagan17 <l_flanagan@coloradocollege.edu>
2023-12-05 09:27:39 -0700

    FULLY WORKING AUTHENTICATION

--- app/server/cas.js
+++ app/server/cas.js
@@ -1,24 +1,25 @@
-const http = require('http');
-const httpCasClient = require('http-cas-client');
+// script from GitHub repo: https://github.com/produck/cas-client/blob/master/README.md 
 
-const handler = httpCasClient({
+const http = require('http');
+const httpCasClient = require('http-cas-client'); //import the cas module from above repo 
+
+const handler = httpCasClient({ // creating a handler with my specific configurations 
   casServerUrlPrefix: 'https://cas.coloradocollege.edu/cas',
   serverName: 'https://tigerlist.vercel.app'
 });
 
+
 const casHandler = async (req, res) => {
   if (!await handler(req, res)) {
-    return false; // Return false to indicate authentication failure
+    return false; // return false to indicate authentication failure
   }
 
-  // Additional logic...
+  // additional logic...
   const { principal, ticket } = req;
   console.log(principal, ticket);
   // { user: 'test', attributes: { ... } }
 
-  // Your statements...
-
-  return true; // Return true to indicate successful authentication
+  return true; // return true to indicate successful authentication
 };
 
-module.exports = casHandler;
+module.exports = casHandler; // export the casHandler function to make available for server.js
