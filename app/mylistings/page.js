fc5dc60d2afe11c740605fea2f02fbaabf962f76
apiccaro <apiccaro@coloradocollege.edu>
2023-12-13 14:23:06 -0700

    added loading for pages and set time for mylisting

--- app/mylistings/page.js
+++ app/mylistings/page.js
@@ -3,14 +3,15 @@
 import SellProducts from "@/Components/SellProducts";
 import Link from "next/link";
 import {useState,useEffect} from 'react'; 
-import Loading from "@/app/mylistings/loading";
+import { Suspense } from "react";
+import Loading from "../loading";
+
 const getUser= async()=>{
 
     const response = await fetch("http://localhost:3000/api/getUser",{
       method:"GET",
       });
     const data = await response.json();
-    console.log(data)
     return data;
 }
 
@@ -29,26 +30,50 @@ const getAllModeratedUsers = async () => {
 export default function Home() {
     const [moderatedUsers, setModeratedUsers] = useState();
     const [isModerated,setIsModerated]=useState();
-    useEffect(() => {
+    const [loading, setLoading] = useState(false);
+    // useEffect(() => {
       
+    //     const fetchData = async () => {
+          
+    //       try {
+    //         const data = await getAllModeratedUsers();
+    //         setModeratedUsers(data.title);
+    //         const user=await getUser();
+    //         const userModerated = data.title.includes(user);
+    //         console.log(user);
+    //         setIsModerated(userModerated);
+    //       } catch (error) {
+    //         console.log(error)
+    //       }
+    //     };
+    
+    //     fetchData();
+    //   }, []);
+
+
+    useEffect(() => {
         const fetchData = async () => {
           
-          try {
-            const data = await getAllModeratedUsers();
-            setModeratedUsers(data.title);
-            const user=await getUser();
-            const userModerated = data.title.includes(user);
-            console.log(user);
-            setIsModerated(userModerated);
-          } catch (error) {
-            console.log(error)
+            try {
+              setLoading(true);
+                const data = await getAllModeratedUsers();
+                setModeratedUsers(data.title);
+                const user = await getUser();
+                const userModerated = data.title.includes(user);
+                console.log(user);
+                setIsModerated(userModerated);
+            } catch (error) {
+                console.log(error);
+            } finally {
+                setTimeout(() => {
+                  setLoading(false);
+              }, 1500);
           }
-          debugger;
-          
-        };
-    
-        fetchData();
-      }, []);
+      };
+
+      fetchData();
+  }, []);
+
     const RowStyle={
         display: 'flex', 
         flexDirection: 'row',
@@ -60,18 +85,50 @@ export default function Home() {
     //const currentUser = 's_treat@coloradocollege.edu'
 
     const showFlaggedPostsLink = moderators.includes(currentUser);
-
     return (
-      <main>
-        <div className="flex flex-col bg-yellow-600 min-h-screen">
-            <div style={RowStyle}>
-                <div className="text-4xl text-black font-semibold p-5">My Listings</div><br></br>
-                {isModerated && (
-                    <Link href={'/flaggedposts'} className="text-4xl text-black font-semibold p-5">Flagged Posts</Link>
-                )}
-            </div>
-            <SellProducts />
-        </div>
-        </main>
-        );
+      <div>
+
+    
+      {loading&& <Loading/>}
+          <main>
+            
+              {!loading&&<div className="flex flex-col bg-yellow-600 min-h-screen">
+                  <div style={RowStyle}>
+                      <div className="text-4xl text-black font-semibold p-5">My Listings</div>
+                      <br></br>
+                      {isModerated && (
+                          <Link href={'/flaggedposts'} className="text-4xl text-black font-semibold p-5">
+                              Flagged Posts
+                          </Link>
+                      )}
+                  </div>
+                  <SellProducts />
+              </div>}
+
+          </main>
+          </div>
+  );
 }
+
+
+//     return (
+//       <Suspense fallback={<Loading/>} maxDuration={10000}>
+
+     
+//       <main>
+           
+       
+//         <div className="flex flex-col bg-yellow-600 min-h-screen">
+//             <div style={RowStyle}>
+//                 <div className="text-4xl text-black font-semibold p-5">My Listings</div><br></br>
+//                 {isModerated && (
+//                     <Link href={'/flaggedposts'} className="text-4xl text-black font-semibold p-5">Flagged Posts</Link>
+//                 )}
+//             </div>
+//             <SellProducts />
+//         </div>
+
+//         </main>
+//         </Suspense>
+//         );
+// }
