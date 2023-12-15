31abf5fa101c77000211da222397cfeb15c7699f
lflanagan17 <l_flanagan@coloradocollege.edu>
2023-12-14 21:11:25 -0700

    changing the readme

--- app/page.js
+++ app/page.js
@@ -4,6 +4,9 @@ import BuyProducts from "@/Components/BuyProducts";
 export default function Home() {
     return (
         <div className="flex flex-row bg-yellow-600 min-h-screen">
+            <head>
+                <title>TigerList</title>
+            </head>
             <Filter />
             <BuyProducts />
         </div>
