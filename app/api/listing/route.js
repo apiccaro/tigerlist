692dcb5947eaf53880d575b052f571aff248ab14
JayMoran <jaymoran103@gmail.com>
2023-12-14 23:00:30 -0700

    got rid of access.js again

--- app/api/listing/route.js
+++ app/api/listing/route.js
@@ -1,7 +1,6 @@
 //const sql = require('mssql');
+//i dont see why we need this but i dont wanna delete a route.js in the api folder. I deleted the "require access.js" so that better not come back from the dead again
 const useDB = false
-const {getValidRequest,testMethod_access} = require('./access.js');
-
 
 import { NextResponse } from 'next/server';
 
