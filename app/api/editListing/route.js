1f65d739d9ce69deaa126734242d4fcfba84685c
JayMoran <jaymoran103@gmail.com>
2023-12-15 00:38:41 -0700

    successful queries for posting and editing

--- app/api/editListing/route.js
+++ app/api/editListing/route.js
@@ -1,68 +1,24 @@
 import { NextResponse } from 'next/server';
 export async function PUT(request){
-    //Get request as json. Might need to cast something or ensure postDict is a dictionary  
-
-    const postDict = await request.json()//.listing or some bs
-
-    //Debug prints
-    //console.log("Attempting to add a listing with content:\n"+postDict+"\n")
-    console.log("\npostDict.title: "+postDict.title+"\n")
-    console.log("\npostDict.listing.title: "+postDict.listing.title+"\n")
-    console.log("\npostDict['title']: "+postDict['title']+"\n")
-    //console.log("\nAs string:\n"+JSON.stringified(postDict)+"\n")
-    //console.log("\nTitle with '.': "+postDict.title+"\n")
-    //console.log("\nTitle with '[]': "+postDict['title']+"\n")
-
+    
+    const postDict = await request.json()
 
     //Build query. Will make this cleaner with $ once this works.
-    // const queryText = "INSERT INTO PostTable "
-    // +"(title, price, description, "
-    // +"category, condition, location, "
-    // +"email, phone, active,"
-    // +" flagged, moderator_ban) "
-
-    // +"VALUES ('"+postDict['title']+"', '"+postDict['title']+"', '"+postDict['title']+"', "
-    // +"'"+postDict['title']+"', '"+postDict['title']+"', '"+postDict['title']+"', "
-    // +"'"+postDict['title']+"', '"+postDict['title']+"', "
-    // +postDict['flagged']+", "+postDict['flagged']+", "+postDict['flagged']+");"
-//     const queryText = `
-//     INSERT INTO PostTable
-//     (title, price, description, category, condition, location, email, phone, active, flagged, moderator_ban)
-//     VALUES (
-//       '${postDict.title}',
-//       '${postDict.price}',
-//       '${postDict.description}',
-//       '${postDict.category}',
-//       '${postDict.condition}',
-//       '${postDict.location}',
-//       '${postDict.email}',
-//       '${postDict.phone}',
-//       ${postDict.active},
-//       ${postDict.flagged},
-//       ${postDict.moderator_ban}
-//     );
-//   `;
-
-    const queryText = `
-        UPDATE PostTable SET
-        title = '${postDict.title}',
-        price = '${postDict.price}',
-        description = '${postDict.description}',
-        category = '${postDict.category}',
-        condition = '${postDict.condition}',
-        location = '${postDict.location}',  
-        email = '${postDict.email}',
-        phone = '${postDict.phone}',
-        active = ${postDict.active},
-        flagged = ${postDict.flagged},
-        moderator_ban = ${postDict.moderator_ban}
-        WHERE id = ${postId};
-        `;
-  
-
-
-
-
+    const queryText = (
+            "UPDATE PostTable SET"
+            + "title = '" + postDict['title'] + "', "
+            + "price = '" + postDict['price'] + "', "
+            + "description = '" + postDict['description'] + "', "
+            + "category = '" + postDict['category'] + "', "
+            + "condition = '" + postDict['condition'] + "', "
+            + "location = '" + postDict['location'] + "', "
+            + "email = '" + postDict['email'] + "', "
+            + "phone = '" + postDict['phone'] + "', "
+            + "active = '" + postDict['active'] + "', "
+            + "flagged = " + postDict['flagged'] + ", "
+            + "moderator_ban = " + postDict['moderator_ban'] + " "
+            + "WHERE post_key = '" + post_key + "';"
+        )
 
     //From here down should be pretty much the same for every route.js (unless we're getting something)
 
--- app/api/putListing/route.js
+++ app/api/putListing/route.js
@@ -1,54 +1,27 @@
 import { NextResponse } from 'next/server';
 
 export async function PUT(request){
-    //Get request as json. Might need to cast something or ensure postDict is a dictionary  
-
-    const postDict = await request.json()//.listing or some bs
+    //converts listing from string to json
+    const postDict = await request.json()
 
     //Debug prints
-    //console.log("Attempting to add a listing with content:\n"+postDict+"\n")
-    console.log("\npostDict.title: "+postDict.title+"\n")
-    console.log("\npostDict.listing.title: "+postDict.listing.title+"\n")
-    console.log("\npostDict['title']: "+postDict['title']+"\n")
-    //console.log("\nAs string:\n"+JSON.stringified(postDict)+"\n")
-    //console.log("\nTitle with '.': "+postDict.title+"\n")
-    //console.log("\nTitle with '[]': "+postDict['title']+"\n")
+    // console.log("\npostDict.title: "+postDict.title+"\n")
+    // console.log("\npostDict.phone: "+postDict.phone+"\n")
+    //console.log("\npostDict.price: "+postDict.price+"\n")
+    //console.log("\npostDict.category: "+postDict.category+"\n")
 
+    //Build query string - Need to change format so certain input characters don't break it. 
+   const queryText = "INSERT INTO PostTable "
+    +"(title, price, description, "
+    +"category, condition, location, "
+    +"email, phone, active, "
+    +"flagged, moderator_ban) "
+    +"VALUES ('"+postDict.title+"', '"+postDict.price+"', '"+postDict.description+"', "
+    +"'"+postDict.category+"', '"+postDict.condition+"', '"+postDict.location+"', "
+    +"'"+postDict.email+"', '"+postDict.phone+"', '"+postDict.active+"', "
+    +postDict.flagged+", "+postDict.flagged+");"
 
-    //Build query. Will make this cleaner with $ once this works.
-    // const queryText = "INSERT INTO PostTable "
-    // +"(title, price, description, "
-    // +"category, condition, location, "
-    // +"email, phone, active,"
-    // +" flagged, moderator_ban) "
-
-    // +"VALUES ('"+postDict['title']+"', '"+postDict['title']+"', '"+postDict['title']+"', "
-    // +"'"+postDict['title']+"', '"+postDict['title']+"', '"+postDict['title']+"', "
-    // +"'"+postDict['title']+"', '"+postDict['title']+"', "
-    // +postDict['flagged']+", "+postDict['flagged']+", "+postDict['flagged']+");"
-    const queryText = `
-    INSERT INTO PostTable
-    (title, price, description, category, condition, location, email, phone, active, flagged, moderator_ban)
-    VALUES (
-      '${postDict.title}',
-      '${postDict.price}',
-      '${postDict.description}',
-      '${postDict.category}',
-      '${postDict.condition}',
-      '${postDict.location}',
-      '${postDict.email}',
-      '${postDict.phone}',
-      ${postDict.active},
-      ${postDict.flagged},
-      ${postDict.moderator_ban}
-    );
-  `;
-  
-
-
-
-
-
+    
     //From here down should be pretty much the same for every route.js (unless we're getting something)
 
     //Make client with credentials
