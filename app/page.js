3bc42b6399db621e967f7fc81b394145a9f72caa
lflanagan17 <l_flanagan@coloradocollege.edu>
2023-12-17 11:42:05 -0700

    fixing tab/hydration errors

--- app/page.js
+++ app/page.js
@@ -4,9 +4,6 @@ import BuyProducts from "@/Components/BuyProducts";
 export default function Home() {
     return (
         <div className="flex flex-row bg-yellow-600 min-h-screen">
-            <head>
-                <title>TigerList</title>
-            </head>
             <Filter />
             <BuyProducts />
         </div>
