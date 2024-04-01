b44cf25933d57c7a9af61e79ab2537b815b0fa50
JayMoran <jaymoran103@gmail.com>
2024-04-01 16:46:15 -0600

    modifying viewall

--- Components/BuyProducts.js
+++ Components/BuyProducts.js
@@ -6,22 +6,7 @@ import Image from "next/image";
 import Link from 'next/link';
 import BuyProductBox from "./BuyProductBox";
 
-
-//test, shouldnt be relevant anymore
-const tryMakeListing = async (listingDict) => {
-  const response = await fetch(process.env.NEXT_PUBLIC_API_CONNECTION_URL+"putListing",{
-    method:"PUT",
-      body : JSON.stringify(
-        listingDict
-      )
-    },
-  )
-
-  const data=await response.json();
-  console.log(data);
-  return data
-};
-
+//Eventually replace with non test api method
 const getNew = async (listingDict) => {
   console.log("Calling getNew in BuyProducts.js")
   const response = await fetch(process.env.NEXT_PUBLIC_API_CONNECTION_URL+"getNewListings",{
@@ -36,68 +21,89 @@ const getNew = async (listingDict) => {
   return data
 };
 
-console.log("About to call getNew in the main body in BuyProducts.js")
-getNew();
 
 
 
 //test, shouldnt be relevant anymore
-const tryDB2 = async () => {
-  console.log ("Called tryDB2")
+// const tryMakeListing = async (listingDict) => {
+//   const response = await fetch(process.env.NEXT_PUBLIC_API_CONNECTION_URL+"putListing",{
+//     method:"PUT",
+//       body : JSON.stringify(
+//         listingDict
+//       )
+//     },
+//   )
 
-  const response = await fetch(process.env.NEXT_PUBLIC_API_CONNECTION_URL+"apiTest",{
-    method:"GET",
-  }).catch(error => console.error('Error: fetch failed in Components/BuyProducts.js: ', error));
+//   const data=await response.json();
+//   console.log(data);
+//   return data
+// };
 
-  console.log ("Finished tryDB2")
-};
+// //test, shouldnt be relevant anymore
+// const tryDB2 = async () => {
+//   console.log ("Called tryDB2")
+
+//   const response = await fetch(process.env.NEXT_PUBLIC_API_CONNECTION_URL+"apiTest",{
+//     method:"GET",
+//   }).catch(error => console.error('Error: fetch failed in Components/BuyProducts.js: ', error));
+
+//   console.log ("Finished tryDB2")
+// };
 
 
-const allPostsFromDB = async () => {
-    const response = await fetch(process.env.NEXT_PUBLIC_API_CONNECTION_URL+"getAllListings",{//original line, replace below once things are working
-      method:"GET",
-      })
-    .then(
-        response => response.json()
-        )
-    .catch(error => console.error('Fetch failed in allPostsFromDB:', error));
+// const allPostsFromDB = async () => {
+//     const response = await fetch(process.env.NEXT_PUBLIC_API_CONNECTION_URL+"getAllListings",{//original line, replace below once things are working
+//       method:"GET",
+//       })
+//     .then(
+//         response => response.json()
+//         )
+//     .catch(error => console.error('Fetch failed in allPostsFromDB:', error));
     
 
-    const data = response;
+//     const data = response;
     
-    //console.log("Data from API: ",data);
+//     //console.log("Data from API: ",data);
 
-    return data.rows;
+//     return data.rows;
 
-};
+// };
 
 
 var newDBlistings = [];
 
-(async () => {
-  console.log("Starting IIFE in BuyProducts.js")
+// (async () => {
+//   console.log("Starting IIFE in BuyProducts.js")
 
-  console.log("Testing tryDB2")
-  await tryDB2()
+//   console.log("Testing tryDB2")
+//   await tryDB2()
 
-  console.log("Testing allPostsFromDB()")
-  let dbcontent = await allPostsFromDB()
-  if (dbcontent !== undefined){
-    newDBlistings = dbcontent
-  }
+//   console.log("Testing allPostsFromDB()")
+//   let dbcontent = await allPostsFromDB()
+//   if (dbcontent !== undefined){
+//     newDBlistings = dbcontent
+//   }
 
-  console.log("Finishing IIFE in BuyProducts.js")
-})();
+//   console.log("Finishing IIFE in BuyProducts.js")
+// })();
 
-if (newDBlistings.lenth < 0){
-  console.log("For now, we're not displaying all content, but here's one dict:")
-  console.log(newDBlistings[0])
-}
 
 //Current hardcoded listing dictionaries. Replacing with queried data once api methods work
 var allListings = [{listingID: "123", title: "Proof of concept", price: "$5", description: "Ticket for Friday's game against North Dakota", category: "Service", condition: "New", location: "Off Campus", email: "student1@coloradocollege.edu", phone: "1234567890", images: ["/testimage3.jpeg", "/testimage4.jpeg"], flagged: false},
                     {listingID: "456", title: "Jacket", price: "$30", description: "Brown leather jacket. Good condition. Size medium.", category: "Clothing", condition: "Used-Good", location: "West Campus", email: "student2@coloradocollege.edu", phone: "2071233333", images: ["/testimage1.jpeg", "/testimage5.jpeg", "/testimage2.jpeg"], flagged: false},
                     {listingID: "789", title: "Carpool", price: "$15", description: "Driving to DIA Wednesday at 2:30pm.", category: "Carpool", condition: "New", location: "East Campus", email: "student3@coloradocollege.edu", phone: "5555552222", images: [""], flagged: true}]
+
+//Try getting new listings from API
+newDBlistings = await getNew()
+
+if (newDBlistings.length > 0){
+  console.log("For now, we're not displaying all content, but here's the first listing:")
+  allListings = newDBlistings;
+  console.log(newDBlistings[0])
+}
+else {
+  console.log("newDBlistings.length = 0, nothing to add to listings array:")
+}
 
 const ProductsGridStyle={
     marginTop: '20px',
