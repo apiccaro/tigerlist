ab8561031dd563a3f11d8a77af89cb9cd0cca4e9
JayMoran <jaymoran103@gmail.com>
2024-04-03 12:51:08 -0600

    testing nodemailer with api/apiTest

--- app/api/apiTest/route.js
+++ app/api/apiTest/route.js
@@ -1,19 +1,23 @@
 import { NextResponse } from 'next/server';
 
+const {sendDefault} = require ('./../../moderation/sendMail');
+
+
 export async function GET(request){
-
     console.log("Using apiTest/route.js (GET) to test basic API functionality")
-
     return NextResponse.json('sample response text (GET method)')
-
 }
 
+// export async function POST(request){
+//     console.log("Using apiTest/route.js (POST) to test basic API functionality") 
+//     return NextResponse.json('sample response text (POST method)')
+// }
+
+
 export async function POST(request){
-
-    console.log("Using apiTest/route.js (POST) to test basic API functionality") 
-
-    return NextResponse.json('sample response text (POST method)')
-
+    console.log("Using apiTest to try and send an email") 
+    sendDefault();
+    return NextResponse.json('response text')
 }
 
 
---
+++ app/moderation/sendMail.js
@@ -0,0 +1,36 @@
+const nodemailer = require ('nodemailer');
+//const handlebars = require ('handlebars');
+const fs = require('fs');
+
+//create transporter instance with app credentials
+const transporter = nodemailer.createTransport({
+    host:'smtp.gmail.email',
+    secure:false,
+    service:'gmail',
+    auth:{
+        user: "tigerlistmoderation@gmail.com",
+        pass: 'SuperSecretPassword5'
+    },
+})
+
+//define email content. Eventually ill want an html template file for nicer emails
+const mailOptions = {
+    from: 'tigerlistmoderation@gmail.com',
+    to: 'jaymoran103@gmail.com',
+    subject: 'Hello from app/moderation/sendMail.js!',
+    text: 'This would be sick'
+};
+
+export default function sendDefault() {
+
+    return (
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('Email sent successfully: ' + info.response);
+            }
+        })
+    );
+
+}
